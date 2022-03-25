3c6a68e364ea3801985dc7282423e01bcca2193e
ELCOT <ELCOT@Lenovo>
2022-03-25 19:39:42 +0530

    first commit

---
+++ classTest/src/classTest/MethodOverloadingSeries.java
@@ -0,0 +1,38 @@
+package classTest;
+
+public class MethodOverloadingSeries {
+
+	void Series(int x,int n)
+	{
+		double s=0;
+		for(int i=0;i<=n;i++)
+		{
+			s=s+(Math.pow(x, i));
+			System.out.print(s+",");
+		}
+	}
+	void Series() {
+		float s=0;
+		for(int i=2;i<=10;i++)
+		{
+			s=s+((float)1/i);
+		}
+		System.out.println("sum= "+s);
+	} void Series(int p)
+	{
+		double s=0;
+		for(int i=0;i<=5;i++)
+		{
+			s=(i*i*i)-1;
+		}
+		System.out.println("sum= "+s);
+	}
+	public static void main(String[] args) {
+		 MethodOverloadingSeries a=new  MethodOverloadingSeries();
+		 a.Series();
+		 a.Series(6);
+		 a.Series(2, 5);
+
+	}
+
+}
---
+++ classTest/src/classTest/String.java
@@ -0,0 +1,10 @@
+package classTest;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		
+
+	}
+
+}
